3c84a1e306271b4d534db6f89ab3a60ddf829555
PdskhnWeb <pdskhn.e@mail.ru>
2024-07-15 07:18:54 +0300

    fix: portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+interface Project {
+  text: string;
+  description: string;
+  id: number;
+  image: string;
+}
 @Component({
   selector: 'app-portfolio',
   standalone: true,
@@ -11,18 +16,20 @@ import { Component, OnInit } from '@angular/core';
 export class PortfolioComponent implements OnInit {
   title = 'Выполненные проекты'
   projects = [
-    { text: 'Кейс Риммерг', image: '/assets/home4.jpg' },
-    { text: 'Кейс Санжей', image: '/assets/home4.jpg' },
-    { text: 'Кейс ВЕСТA_HOUSE', image: '/assets/home4.jpg' },
-    { text: 'Кейс Линя ПЕРЕУЛОК', image: '/assets/home4.jpg' },
-    { text: 'Кейс Пятничные Огни', image: '/assets/home4.jpg' },
-    { text: 'Кейс Пятничные Огни', image: '/assets/home4.jpg' },
-    { text: 'Кейс Солнечный Утро', image: '/assets/home2.jpg' }
-    // Добавьте больше проектов при необходимости
+    { text: 'Кейс Риммерг', description: "Долгосрочная аренда", id: 1, image: '/assets/home4.jpg' },
+    { text: 'Кейс Санжей', description: "Долгосрочная аренда", id: 2, image: '/assets/home4.jpg', },
+    { text: 'Кейс ВЕСТA_HOUSE', description: "Долгосрочная аренда", id: 3, image: '/assets/home4.jpg' },
+    { text: 'Кейс Линя ПЕРЕУЛОК', description: "Долгосрочная аренда", id: 4, image: '/assets/home4.jpg' },
+    { text: 'Кейс Пятничные Огни', description: "Долгосрочная аренда", id: 5, image: '/assets/home4.jpg' },
+    { text: 'Кейс Пятничные Огни', description: "Долгосрочная аренда", id: 6, image: '/assets/home4.jpg' },
+    { text: 'Кейс Солнечный Утро', description: "Долгосрочная аренда", id: 7, image: '/assets/home2.jpg' }
   ];
 
-  constructor() { }
+  showDescription: { [key: number]: boolean } = {};
 
+  constructor() {
+    this.projects.forEach(project => this.showDescription[project.id] = false);
+  }
   ngOnInit(): void {
   }
 }
